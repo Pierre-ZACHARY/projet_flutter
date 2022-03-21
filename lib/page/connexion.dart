998f1d1d2cea92040e46709f5050a7d52266f4ce
nouailledegorcepaul <PNDikckdk72!!>
2022-03-21 23:10:28 +0100

    Connexion Page terminée visuellement.

--- lib/page/connexion.dart
+++ lib/page/connexion.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:projet_flutter/utilities/constant.dart';
 
 class ConnexionPage extends StatefulWidget {
@@ -11,6 +12,7 @@ class ConnexionPage extends StatefulWidget {
 
 class _ConnexionPageState extends State<ConnexionPage> {
 
+  bool rememberMe = false;
   Future<void> Login(String email, String password) async {
     try {
       UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
@@ -52,7 +54,7 @@ class _ConnexionPageState extends State<ConnexionPage> {
           "Email",
           style: kLabelStyle,
         ),
-        SizedBox(height: 10.0),
+        const SizedBox(height: 10.0),
         Container(
           alignment: Alignment.centerLeft,
           decoration: kBoxDecorationStyle,
@@ -86,7 +88,7 @@ class _ConnexionPageState extends State<ConnexionPage> {
           "Password",
           style: kLabelStyle,
         ),
-        SizedBox(height: 10.0),
+        const SizedBox(height: 10.0),
         Container(
           alignment: Alignment.centerLeft,
           decoration: kBoxDecorationStyle,
@@ -124,76 +126,201 @@ class _ConnexionPageState extends State<ConnexionPage> {
       ),
     );
   }
+
+  Container makeLogInButton(){
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 25.0),
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: () => print("Do nothing"),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            padding:  MaterialStateProperty.all<EdgeInsetsGeometry>(const EdgeInsets.all(15.0)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+            ),
+          ),
+          child: const Text(
+            "Log in",
+            style: TextStyle(
+                color: Color(0xFF154da4),
+                letterSpacing: 2.0,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: "OpenSans"
+            ),
+          ),
+        )
+    );
+  }
+
+  Column makeSignInText(){
+    return Column(
+      children: const <Widget>[
+        Text(
+          "- Or -",
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Text(
+            "Sign in with",
+            style: kLabelStyle)
+      ],
+    );
+  }
+
+  GestureDetector makeSignInWithGoogleButton(){
+    return GestureDetector(
+      onTap: () => print("Login with Google"),
+      child: Container(
+        height: 60,
+        width: 60,
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black,
+                  offset: Offset(0, 2),
+                  blurRadius: 6.0
+              )
+            ],
+            image: DecorationImage(
+              image: AssetImage("assets/logos/google.jpg"),
+            )
+        ),
+      ),
+    );
+  }
+
+  Row makeRememberMeRow(){
+    return Row(
+      children: <Widget>[
+        Theme(
+          data: ThemeData(unselectedWidgetColor: Colors.white),
+          child: Checkbox(
+            value: rememberMe,
+            checkColor: Colors.green,
+            activeColor: Colors.white,
+            onChanged: (value) {
+              setState(() {
+                rememberMe = value!;
+              });
+            },
+          ),
+        ),
+        const Text(
+          "Remember me",
+          style: kLabelStyle,
+        ),
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
     Login("pierre.zachary45@gmail.com", "password");
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Route'),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFF1d9586),
-                  Color(0xFF154da4),
-                ],
-                stops: [0, 1],
-              )
-            ),
-          ),
-          Container(
-            height: double.infinity,
-            child: SingleChildScrollView(
-              physics: const AlwaysScrollableScrollPhysics(),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 40.0,
-                vertical: 120.0,
+      // appBar: AppBar(
+      //   title: const Text('First Route'),
+      // ),
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xFF1d9586),
+                      Color(0xFF154da4),
+                    ],
+                    stops: [0, 1],
+                  )
+                ),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    "Sign in",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "OpenSans",
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+              SizedBox(
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40.0,
+                    vertical: 120.0,
                   ),
-                  const SizedBox(height: 30.0),
-                  makeEmailEntry(),
-                  const SizedBox(height: 30.0),
-                  makePasswordEntry(),
-                  makePasswordForgotButton(),
-                  Container(
-                    child: ElevatedButton(
-                      onPressed: () => print("Do nothing"),
-                      child: const Text(
-                        "Login",
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Sign in",
                         style: TextStyle(
                           color: Colors.white,
-                          letterSpacing: 2.0,
-                          fontSize: 18.0,
+                          fontFamily: "OpenSans",
+                          fontSize: 30.0,
                           fontWeight: FontWeight.bold,
-                          fontFamily: "OpenSans"
                         ),
                       ),
-                    )
+                      const SizedBox(height: 30.0),
+                      makeEmailEntry(),
+                      const SizedBox(height: 30.0),
+                      makePasswordEntry(),
+                      makePasswordForgotButton(),
+                      makeRememberMeRow(),
+                      makeLogInButton(),
+                      makeSignInText(),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 30.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            makeSignInWithGoogleButton(),
+                            // A modifier avec logo Apple pour la connexion
+                            makeSignInWithGoogleButton(),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () => print("Sign up tap"),
+                        child: RichText(
+                            text: const TextSpan(
+                                children: [
+                                    TextSpan(
+                                        text: "No account ? ",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.w400
+                                        )
+                                    ),
+                                    TextSpan(
+                                        text: "Sign Up",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold,
+                                        )
+                                    )
+                                ]
+                            )
+                        ),
+                      )
+                    ],
                   )
-                ],
+                ),
               )
-            ),
-          )
 
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
